8d1aa339bf8a052df1b904ddb18b32c2079e34b0
hiroyuki.obinata <hiroyuki.obinata@gmail.com>
2022-10-25 22:58:39 +0900

    support camera

--- yolov7main.cpp
+++ yolov7main.cpp
@@ -16,6 +16,7 @@
 
 #include "yolo.h"
 
+#include <opencv2/opencv.hpp>
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
@@ -39,20 +40,68 @@ int main(int argc, char** argv)
 
     yolov7.load(target_size);
 
-    cv::Mat m = cv::imread(imagepath, 1);
-    if (m.empty())
+    cv::Mat m;
+    cv::VideoCapture cap(0, cv::CAP_V4L2);
+    cap.set(cv::CAP_PROP_FOURCC, cv::VideoWriter::fourcc('Y','U','Y','V'));
+    cap.set(cv::CAP_PROP_FRAME_WIDTH, 640);
+    cap.set(cv::CAP_PROP_FRAME_HEIGHT, 480);
+    cap.set(cv::CAP_PROP_FPS, 25);
+    if (!cap.isOpened())
     {
-        fprintf(stderr, "cv::imread %s failed\n", imagepath);
+        std::cerr << "Error opening video file\n";
         return -1;
     }
 
-    std::vector<Object> objects;
-    yolov7.detect(m, objects);
-    yolov7.draw(m, objects);
+    auto start = std::chrono::high_resolution_clock::now();
+    int frame_count = 0;
+    float fps = -1;
+    int total_frames = 0;
 
-    cv::imshow("YoloV7",m);
-//    cv::imwrite("test.jpg",m);
-    cv::waitKey(0);
+    while (true)
+    {
+        cap.read(m);
+        if (m.empty())
+        {
+            std::cout << "End of stream\n";
+            break;
+        }
 
+
+        std::vector<Object> objects;
+        yolov7.detect(m, objects);
+        yolov7.draw(m, objects);
+
+        frame_count++;
+        total_frames++;
+
+        if (frame_count >= 30)
+        {
+
+            auto end = std::chrono::high_resolution_clock::now();
+            fps = frame_count * 1000.0 / std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+
+            frame_count = 0;
+            start = std::chrono::high_resolution_clock::now();
+        }
+
+        if (fps > 0)
+        {
+
+            std::ostringstream fps_label;
+            fps_label << std::fixed << std::setprecision(2);
+            fps_label << "FPS: " << fps;
+            std::string fps_label_str = fps_label.str();
+
+            cv::putText(m, fps_label_str.c_str(), cv::Point(10, 25), cv::FONT_HERSHEY_SIMPLEX, 1, cv::Scalar(0, 0, 255), 2);
+        }
+
+        cv::imshow("YoloV7",m);
+        if (cv::waitKey(1) != -1)
+        {
+            cap.release();
+            std::cout << "finished by user\n";
+            break;
+        }
+    }
     return 0;
 }
